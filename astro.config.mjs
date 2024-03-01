2b12e3a0270a7b6b4fe14475e9478c1fb32a079f
JsPark <junseongpark.dev@gmail.com>
2024-03-01 10:38:34 +0900

    update layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jspark.dev",
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap(), mdx(), icon()]
 });
