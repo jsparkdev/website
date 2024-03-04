ea8e56e07c2c8744689d79458f94ae639d2ebb1f
JsPark <junseongpark.dev@gmail.com>
2024-03-04 15:00:17 +0900

    feat: add 404 page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
   theme: {
     extend: {},
-    //surface: 900 (bottom) ~ 600 (top)
+    // surface: 900 (bottom) ~ 600 (top)
     // text: 200 (headings), 300 (hover), 400 (body),
     // accent: sky 400
   },
