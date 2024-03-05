160812c64e8ebbe0394ef1d4342637be2af4d79a
Junseong Park <junseongpark.dev@gmail.com>
2024-03-05 09:22:02 +0900

    style: add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
-  theme: {
-    extend: {},
-    // surface: 900 (bottom) ~ 600 (top)
-    // text: 200 (headings), 300 (hover), 400 (body),
-    // accent: sky 400
-  },
   plugins: [require("@tailwindcss/typography")],
 };
