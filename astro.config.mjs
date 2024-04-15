8feebd14a9f79b24e4a7f20131bf9ec3028e6372
Junseong Park <junseongpark.dev@gmail.com>
2024-04-15 14:44:51 +0900

    chore: update Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import {
-	transformerNotationDiff,
-	transformerNotationErrorLevel,
-} from "@shikijs/transformers";
+import { transformerNotationDiff, transformerNotationErrorLevel } from "@shikijs/transformers";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jspark.dev",
-	markdown: {
-		syntaxHighlight: "shiki",
-		shikiConfig: {
-			theme: "aurora-x",
-			transformers: [
-				transformerNotationDiff(),
-				transformerNotationErrorLevel(),
-			],
-		},
-	},
-	integrations: [tailwind({ applyBaseStyles: false }), sitemap(), mdx()],
+  site: "https://jspark.dev",
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "aurora-x",
+      transformers: [transformerNotationDiff(), transformerNotationErrorLevel()]
+    }
+  },
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), icon()]
 });
