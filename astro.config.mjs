8d53cb120be18c50cd3af8c35dba0099bdcb8312
Junseong Park <junseongpark.dev@gmail.com>
2024-06-13 00:24:23 +0900

    update main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import sitemap from '@astrojs/sitemap'
 import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
