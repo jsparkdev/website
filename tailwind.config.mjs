dc17d7ae968b67ad2551b80a78f9ad2add0d890b
Junseong Park <junseongpark.dev@gmail.com>
2024-06-22 20:23:30 +0900

    style: update style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,21 @@ export default {
 			maxWidth: {
 				800: '800px',
 			},
+			keyframes: {
+				'slide-up': {
+					from: {
+						translate: '0 12px',
+						opacity: '0',
+					},
+					to: {
+						translateY: '0 0',
+						opacity: '1',
+					},
+				},
+			},
+			animation: {
+				'slide-up': 'slide-up 0.8s ease-out backwards',
+			},
 		},
 		fontFamily: {
 			sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
