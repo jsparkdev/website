0cad573cd7a05f6c8adfdb2fd7d81fe2818bdb55
Junseong Park <junseongpark.dev@gmail.com>
2024-06-30 13:27:45 +0900

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
 	site: 'https://jspark.dev',
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
 	integrations: [
 		sitemap(),
 		tailwind({
