a00baeab715fe549b2dc75338ffa0a3340dcdd3c
Junseong Park <junseongpark.dev@gmail.com>
2024-06-30 13:34:03 +0900

    fix: wrong adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
