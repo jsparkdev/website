0dac638ab8598b0ee71e65b23d54b50bf2e062d1
Junseong Park <junseongpark.dev@gmail.com>
2024-07-15 21:08:06 +0900

    chore: update deps

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+	content: ['./src/**/*.{astro,tsx}'],
 	theme: {
 		extend: {
 			maxWidth: {
