7d8a478182747edaf73c3f9efaeb242451ffee5a
Junseong Park <junseongpark.dev@gmail.com>
2024-10-02 09:03:09 +0900

    style(font): update to variable

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,6 @@
-// @ts-check
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://jspark.dev",
   integrations: [sitemap()],
