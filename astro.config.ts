9c15703e7ab9a7ecd2baf3ea4f4c3c3e0e2e9e71
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-02-18 14:41:00 +0900

    expressive code

--- astro.config.ts
+++ astro.config.ts
@@ -2,19 +2,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode from "astro-expressive-code";
 
 export default defineConfig({
   site: "https://jspark.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    sitemap(),
+    expressiveCode({
+      themes: ["andromeeda"],
+      styleOverrides: { codeFontFamily: "JetBrains Mono" },
+    }),
+    mdx(),
+  ],
   vite: {
     plugins: [tailwindcss()],
     build: {
       target: "esnext",
-    },
-  },
-  markdown: {
-    shikiConfig: {
-      theme: "andromeeda",
     },
   },
   experimental: {
